2a085536d6c5b3c609b4b1a7e5f07cee0586b6e1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-18 17:55:34 -0400

    updated index.php

--- index.php
+++ index.php
@@ -7,23 +7,5 @@
 </head>
 <body>
     <p>Welcome to My Website</p>
-    <?php
-    // Here we output a string into the browser
-    /*    This is a multi-line comment.
-        It can span multiple lines. */
-    if(true) {
-    echo "Hello, World!";
-}
-    ?>
-    <?php if (true) {?>
-        <p>This is some HTML text.</p>
-        <p>Another <?php echo "awesome" ?> line of text.</p>
-    <?php } ?>
-
-    <?php 
-    //declaring a variable
-    $greeting = "Hello, User!";
-    echo $greeting;
-    ?>
 </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Welcome to My Website</p>
+    <?php
+    // Here we output a string into the browser
+    /*    This is a multi-line comment.
+        It can span multiple lines. */
+    if(true) {
+    echo "Hello, World!";
+}
+    ?>
+    <?php if (true) {?>
+        <p>This is some HTML text.</p>
+        <p>Another <?php echo "awesome" ?> line of text.</p>
+    <?php } ?>
+
+    <?php 
+    //declaring a variable
+    $greeting = "Hello, User!";
+    echo $greeting;
+    ?>
+</body>
+</html>
