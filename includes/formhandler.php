31b309be2e130d173342907eebb3e31d1dccff11
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-19 18:08:04 -0400

    updated formhandler

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -8,11 +8,20 @@ if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['fav
         $lastname = htmlspecialchars(trim($_POST['lastname']));
         $favouritepet = htmlspecialchars(trim($_POST['favouritepet']));
 
+
+        if (empty($firstname) || empty($lastname) || empty($favouritepet)) {
+            echo "All fields are required.";
+            exit();
+        }
+
         // Process the data (e.g., save to database, send email, etc.)
         // For demonstration, we'll just echo the values
         echo "First Name: $firstname<br>";
         echo "Last Name: $lastname<br>";
         echo "Favourite Pet: $favouritepet<br>";
+
+        //header("Location: ../index.php?success=1");
+        header("Location: ../index.php");
     } else {
         echo "Form not submitted correctly.";
     }
