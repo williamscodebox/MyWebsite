848c86708c342e04c0fa01fe3278d38403384b16
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-19 21:20:34 -0400

    updated index

--- index.php
+++ index.php
@@ -11,23 +11,32 @@
 </head>
 <body>
 <main class="main flex flex-col items-center justify-center">
-    <form action="includes/formhandler.php" method="post">
-        <label for="firstname">FirstName?</label>
-        <input class="input" type="text" id="firstname" name="firstname" placeholder="Firstname..." required>
+    <div>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">FirstName?</label>
+            <input class="input" type="text" id="firstname" name="firstname" placeholder="Firstname..." required>
 
-        <label for="lastname">LastName?</label>
-        <input class="input" type="text" id="lastname" name="lastname" placeholder="Lastname..." required>
+            <label for="lastname">LastName?</label>
+            <input class="input" type="text" id="lastname" name="lastname" placeholder="Lastname..." required>
 
-        <label for="favouritepet">Favourite Pet?</label>\
-        <select class="input id="favouritepet" name="favouritepet">
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="fish">Fish</option>
-            <option value="bird">Bird</option>
-        </select>
+            <label for="favouritepet">Favourite Pet?</label>\
+            <select class="input id="favouritepet" name="favouritepet">
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="fish">Fish</option>
+                <option value="bird">Bird</option>
+            </select>
+            
+            <button class="button" type="submit">Submit</button>
         
-        <button class="button" type="submit">Submit</button>
-    </form>
+        </form>
+    </div>
+    <div> 
+        <p>Example Form Submission</p>
+        <p>Fill out the form above and submit to see the results.</p>
+        <p>The second button is just an example for css purposes</p>
+        <button type="submit">Submit</button>
+    </div>
 </main>
 </body>
 </html>
