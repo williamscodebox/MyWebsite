16a7c7e6bc1cbf360b174bc8949fa0d6e967c7a1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-23 08:41:56 -0400

    updated dbh

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,12 @@
 <?php
+
+$dsn = "mysql:host=localhost;dbname=tutorial";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    $conn = new PDO($dsn, $dbusername, $dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
