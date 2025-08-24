5358f49dd85fe3a12ff83867c353eecb894d823c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-24 07:59:35 -0400

    updated userupdate

--- includes/userupdate.inc.php
+++ includes/userupdate.inc.php
@@ -1,2 +1,37 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];  
+    $email = $_POST['email'];
+
+    try {
+        require_once 'dbh_inc.php';
+       
+        $query = "UPDATE users SET username = :username, pwd = :pwd, email = :email WHERE id = 5;";
+        $stmt = $conn->prepare("$query");
+
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':pwd', $pwd); 
+        $stmt->bindParam(':email', $email);
+        
+        $stmt->execute();
+
+        // code continues here
+
+        $conn = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        die();
+
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
+
+} else {
+
+    header("Location: ../index.php");
+}
