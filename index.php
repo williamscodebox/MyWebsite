8ad33a6de17a580cb7b002c3d8711bd0ab1491fc
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-24 15:56:13 -0400

    updated index

--- index.php
+++ index.php
@@ -8,7 +8,21 @@
     <title>Document</title>
 </head>
 <body>
-    <main class="main flex flex-col items-center justify-center">
+    <main class="page flex flex-row items-center justify-around">
+        
+    <div class="main flex flex-col items-center justify-center">
+        <div class="div">
+        <!-- <h3 class="pb-1" >Search Account</h3> -->
+       
+        <form action="search.php" method="post">
+            <label for="search">Search for user:</label><br>
+            <input class="input" id="search" type="text" name="usersearch" placeholder="Search..."><br>
+            <button type="submit" name="submit">Search</button>
+        </form>
+        </div>
+</div>
+        <div class="main flex flex-col items-center justify-center">
+
         <div class="div">
         <!-- <h3 class="pb-1" >Signup</h3> -->
        
@@ -38,6 +52,8 @@
             <button type="submit" name="submit">Delete</button>
         </form>
         </div>
+</div>
+<div class="main flex flex-col items-center justify-center">
         <div class="div">
         <!-- <h3 class="pb-1" >Search Account</h3> -->
        
@@ -47,6 +63,18 @@
             <button type="submit" name="submit">Search</button>
         </form>
         </div>
+</div>
+<div class="main flex flex-col items-center justify-center">
+        <div class="div">
+        <!-- <h3 class="pb-1" >Search Account</h3> -->
+       
+        <form action="search.php" method="post">
+            <label for="search">Search for user:</label><br>
+            <input class="input" id="search" type="text" name="usersearch" placeholder="Search..."><br>
+            <button type="submit" name="submit">Search</button>
+        </form>
+        </div>
+</div>
     </main>
 </body>
 </html>
