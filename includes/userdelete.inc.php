59a854ed7c1f7b23220165da9e1995735b85dfc3
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-24 08:04:34 -0400

    updated userdelete

--- includes/userdelete.inc.php
+++ includes/userdelete.inc.php
@@ -1,2 +1,35 @@
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];  
+
+    try {
+        require_once 'dbh_inc.php';
+      
+        $query = "DELETE FROM users WHERE username = :username AND pwd = :pwd;";
+        $stmt = $conn->prepare("$query");
+
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':pwd', $pwd); 
+        
+        $stmt->execute();
+
+        // code continues here
+
+        $conn = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        die();
+
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
+
+} else {
+
+    header("Location: ../index.php");
+}
