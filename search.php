0ab4b76798bb56a35a89b54a481912f59e502fb3
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-24 16:51:41 -0400

    updated search

--- search.php
+++ search.php
@@ -1,5 +1,46 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];  
+    $email = $_POST['email'];
+
+    try {
+        require_once 'dbh_inc.php';
+        // with unnamed placeholders
+        // $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);";
+        // $stmt = $conn->prepare("$query");
+        // $stmt->execute([$username, $pwd, $email]);
+
+        // $stmt->execute([$username, $pwd, $email]);
+
+        // with named placeholders
+        $query = "INSERT INTO users (username, pwd, email) VALUES (:username, :pwd, :email);";
+        $stmt = $conn->prepare("$query");
+
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':pwd', $pwd); 
+        $stmt->bindParam(':email', $email);
+        
+        $stmt->execute();
+
+        // code continues here
+
+        $conn = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        die();
+
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
+
+} else {
+
+    header("Location: ../index.php");
+}
 ?>
 
 <!DOCTYPE html>
