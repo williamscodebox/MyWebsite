d8641095f0427dd509bf968e4fb84959c4ab575c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-25 18:36:50 -0400

    updated cofig

--- sessionExample/config.php
+++ sessionExample/config.php
@@ -2,3 +2,13 @@
 
 ini_set("session.use_only_cookies", 1);
 ini_set("session.use_strict_mode", 1);
+
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'path' => '/',
+    'domain' => 'localhost',
+    'secure' => true,
+    'httponly' => true,
+]);
+
+session_start();
