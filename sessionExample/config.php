73ffca62a3849fd15f4cc46bda6fb247cf6c91c7
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-25 18:41:18 -0400

    updated config

--- sessionExample/config.php
+++ sessionExample/config.php
@@ -13,4 +13,15 @@ session_set_cookie_params([
 
 session_start();
 
-session_regenerate_id(true);
+if (!isset($_SESSION['last_regeneration'])) {
+   
+    session_regenerate_id(true);
+    $_SESSION['last_regeneration'] = time();
+} else {
+    $interval = 60 * 30; 
+
+    if (time() - $_SESSION['last_regeneration'] > $interval) {
+        session_regenerate_id(true);
+        $_SESSION['last_regeneration'] = time();
+    }
+}
