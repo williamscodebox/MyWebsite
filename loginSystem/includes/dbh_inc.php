b0e25d3dfde4580553a248da665039c300da1a50
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-26 20:47:22 -0400

    updated dbh

--- loginSystem/includes/dbh_inc.php
+++ loginSystem/includes/dbh_inc.php
@@ -1,14 +1,17 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=tutorial";
+// $dsn = "mysql:host=localhost;dbname=tutorial";
+$host = "localhost";
+$dbname="tutorial";
 $dbusername = "root";
 $dbpassword = "";
 
 try {
-    $conn = new PDO($dsn, $dbusername, $dbpassword);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $conn = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    die("Connection failed: " . $e->getMessage());
 }
 
 // dbh stands for database handler
