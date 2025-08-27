c4635e94092f7e1a66128c48d547432807fe24fb
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-26 20:03:25 -0400

    updated hashpwd

--- includes/hashpwd.inc.php
+++ includes/hashpwd.inc.php
@@ -1,2 +1,17 @@
 <?php
 
+$pwdSignUp = "SaltyPenguin";
+
+$hashedPwd = password_hash($pwdSignUp, PASSWORD_BCRYPT, ['cost' => 12]);
+echo "<br>" . $hashedPwd;
+
+$pwdLogin = "SaltyPenguin";
+
+if (password_verify($pwdLogin, $hashedPwd)) {
+    echo "<br>Password is valid.";
+} else {
+    echo "<br>Password is invalid.";
+}
+
+
+
