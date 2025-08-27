23b46b379ef4e850247db64f7d505c15d7222098
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-26 22:39:18 -0400

    set up mvc for signup

---
+++ loginSystem/includes/signup.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+    $email = $_POST['email'];
+
+    try {
+        require_once 'dbh.inc.php';
+
+        if (emptyInputSignup($username, $pwd, $email) !== false) {
+            throw new Exception("Empty input");
+        }
+
+        if (invalidUid($username) !== false) {
+            throw new Exception("Invalid username");
+        }
+
+        if (invalidEmail($email) !== false) {
+            throw new Exception("Invalid email");
+        }
+
+        if (uidExists($conn, $username, $email) !== false) {
+            throw new Exception("Username or email already taken");
+        }
+    } catch (Exception $e) {
+        $errorMessage = $e->getMessage();
+        die("Query failed: " . $errorMessage);
+    }
+
+    createUser($conn, $username, $pwd, $email);
+} else {
+    header("location: ../index.php");
+    die();
+}
