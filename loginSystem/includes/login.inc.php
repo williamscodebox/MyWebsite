d2335809a1442eb1df7e0873ec8813f8c902acb5
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-29 18:53:48 -0400

    updated index

--- loginSystem/includes/login.inc.php
+++ loginSystem/includes/login.inc.php
@@ -1,8 +1,11 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] === "POST") {
-
+if($_SERVER["REQUEST_METHOD"] === "POST") {   
     
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+
 } else {
     header("location: ../index.php");
     die();
