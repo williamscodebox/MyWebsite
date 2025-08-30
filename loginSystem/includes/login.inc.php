8d55846369242ec425b4e0d00a9e94b22d1a0551
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-29 22:07:39 -0400

    updated login

--- loginSystem/includes/login.inc.php
+++ loginSystem/includes/login.inc.php
@@ -5,6 +5,12 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST['username'];
     $pwd = $_POST['pwd'];
 
+    try {
+        //code...
+    } catch (Exception $e) {
+        $errorMessage = $e->getMessage();
+        die("Query failed: " . $errorMessage);
+    }
 
 } else {
     header("location: ../index.php");
